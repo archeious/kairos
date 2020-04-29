0123f05f7c30e544145e1450501bbc6c8f34d506
Jeff Smith <github@unbiasedgeek.com>
2020-04-29 16:37:15 -0600

    Basic postgres connection

--- main.go
+++ main.go
@@ -10,12 +10,24 @@ import (
 const (
     host        = "localhost"
     port        = 5432
-    user        = "postgres"
+    user        = "kairos_test_rw"
     password    = "not-a-real-password"
     dbname      = "kairos_test"
 )
 
 func main() {
     psqlInfo := fmt.Sprintf("host=%s port=%d user=%s passowrd=%s dbname=%s sslmode=disable",
-        host, port, user, password, dbname)        
+        host, port, user, password, dbname)
+    db, err := sql.Open("postgres", psqlInfo)
+    if err != nil {
+        panic(err)        
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Successfully Connected!")
 }
